f0013a40d422d2ddb1e230392aebfef011cdf041
Munyeshya <obedbenny54@gmail.com>
2025-03-19 16:46:27 +0200

    bug fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -25,7 +25,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! </ThemedText>
+        <ThemedText type="title">Welcome to IAHS </ThemedText>
         <HelloWave />
       </ThemedView>
 
